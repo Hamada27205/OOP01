9f925d7de526ac8e4abd60da03820c142b539de2
Hamada27205 <ahmedfarag8273@gmail.com>
2026-08-04 18:57:50 +0300

    Question01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿namespace AssignmentOOP01
+﻿using AssignmentOOP01.Struct;
+
+namespace AssignmentOOP01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1
+
+            #region Part1
+
+            //DeliveryAddress A1 = new DeliveryAddress();
+            //A1.City = "Cairo ";
+            //A1.Street = "Talaat Harb";
+            //Console.WriteLine(A1.City + "," + A1.Street);
+            //DeliveryAddress A2 = new DeliveryAddress();
+            //A2 = A1;
+            //A2.City = "Giza ";
+            //A2.Street = "Faisal";
+            //Console.WriteLine(A2.City + "," + A2.Street);
+            //Console.WriteLine(A1.City + "," + A1.Street);
+
+
+            #endregion
+
+            #region Part2
+
+            //Customer customer1 = new Customer();
+            //customer1.Name = "Ahmed";
+
+            //Console.WriteLine(customer1.Name);
+            //Customer customer2 = new Customer();
+            //customer1 = customer2;
+            //customer2.Name = "Hamada";
+            //Console.WriteLine(customer2.Name);
+            //Console.WriteLine(customer1.Name);
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
---
+++ Struct/DeliveryAddress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOOP01.Struct
+{
+    #region Q1
+
+    #region Part1
+    //public struct DeliveryAddress
+    //{
+
+    //    public string City;
+    //    public string Street;
+
+    //} 
+    #endregion
+
+    #region Part2
+
+    //public class Customer
+    //{
+    //    public string Name;
+    //}
+
+    #endregion
+
+    #endregion
+}
